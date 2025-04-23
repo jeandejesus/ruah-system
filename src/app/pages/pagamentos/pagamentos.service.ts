0af978ce453ec21344c8e5c1faa9bf32e4a0a41d
jeandejesus <jeanfreirecontato@outlook.com>
2025-04-22 22:45:01 -0300

    correção mensagens de erro

--- src/app/pages/pagamentos/pagamentos.service.ts
+++ src/app/pages/pagamentos/pagamentos.service.ts
@@ -16,17 +16,37 @@ export interface Customer {
   phone: string;
   created: Date;
   status: string;
+  subscriptions: FormattedSubscription[];
 }
 
+export interface FormattedSubscription {
+  id: string;
+  status:
+    | 'incomplete'
+    | 'incomplete_expired'
+    | 'trialing'
+    | 'active'
+    | 'past_due'
+    | 'unpaid'
+    | 'canceled';
+  current_period_end: Date;
+  cancel_at: Date | null;
+  cancellation_reason: string | null;
+  payment_error: string | null; // mensagem detalhada de erro ou expiração
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PagamentosService {
   private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
-  getCustomers(limit: number = 10, startingAfter?: string, searchTerm?: string): Observable<CustomerResponse> {
+  getCustomers(
+    limit: number = 10,
+    startingAfter?: string,
+    searchTerm?: string
+  ): Observable<CustomerResponse> {
     let url = `${this.apiUrl}/stripe/customers-with-status?limit=${limit}`;
     if (startingAfter) {
       url += `&startingAfter=${startingAfter}`;
@@ -36,4 +56,4 @@ export class PagamentosService {
     }
     return this.http.get<CustomerResponse>(url);
   }
-} 
+}
