89c2858974f6f0ad2ccdcf0da44825055a0651ff
jeandejesus <jeanfreirecontato@outlook.com>
2026-03-27 21:05:31 -0300

    ajuste enviriomente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ruah-system-back.onrender.com' // Substitua pela URL do seu backend em produção
+  apiUrl: 'https://api.ruahdanca.com.br' // Substitua pela URL do seu backend em produção
 }; 
